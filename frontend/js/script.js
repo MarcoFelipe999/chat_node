3c2a1ba9008122c400d70f88e1f7ab2bc4e40c73
Marcos Felipe <116011701+MarcoFelipe999@users.noreply.github.com>
2024-06-05 15:15:11 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://chat-mf-8tah.onrender.com")
+    websocket = new WebSocket("wss://chat-mf-8tah.onrender.com")
     websocket.onmessage = processMessage
 }
 
